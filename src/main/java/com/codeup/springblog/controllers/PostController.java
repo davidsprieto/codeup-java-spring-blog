a137ee12dcca6ec2818a11ee7d8d5367212458a6
David Prieto <david.s.prieto1@gmail.com>
2022-06-30 17:00:57 -0500

    Started part 3 of Spring - Fundamentals: views exercises.

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,35 +2,42 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/posts")
-    @ResponseBody
-    public String indexPage() {
-        return "This is the index page!";
-    }
+//    Spring - Fundamentals: Controllers Exercises
 
-    @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewIndividualPost(@PathVariable int id) {
-        return "The id is: " + id + "!";
-    }
+//    @GetMapping("/posts")
+//    @ResponseBody
+//    public String indexPage() {
+//        return "This is the index page!";
+//    }
+//
+//    @GetMapping("/posts/{id}")
+//    @ResponseBody
+//    public String viewIndividualPost(@PathVariable int id) {
+//        return "The id is: " + id + "!";
+//    }
+//
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String viewPostForm() {
+//        return "A form will be here to fill out and post!";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    @ResponseBody
+//    public String createPost() {
+//        return "Create a new post!";
+//    }
 
-    @GetMapping("/posts/create")
-    @ResponseBody
-    public String viewPostForm() {
-        return "A form will be here to fill out and post!";
-    }
+//    Spring - Fundamentals: Views Exercises
 
-    @PostMapping("/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "Create a new post!";
+    @GetMapping ("/posts/{id}")
+    public String viewIndividualPost() {
+
     }
 
 }
