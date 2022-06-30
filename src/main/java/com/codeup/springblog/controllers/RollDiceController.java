4594b46158dd40315082cc1303b7826fbbc36cf5
David Prieto <david.s.prieto1@gmail.com>
2022-06-30 15:03:31 -0500

    Completed Spring: Fundamentals - Views roll dice exercise.

---
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -0,0 +1,32 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String showGuessForm() {
+        return "roll-dice";
+    }
+
+    @PostMapping("/roll-dice")
+    public String showGuess(@RequestParam(name = "guess") int guess, Model model) {
+        Random random = new Random();
+        int dice = random.nextInt(7-1) + 1;
+        model.addAttribute("guess", "You guessed " + guess + "!");
+        if (guess == dice) {
+            model.addAttribute("dice", "The dice rolled on " + dice + "!" + " You win!");
+        } else {
+            model.addAttribute("dice", "The dice rolled on " + dice + "!" + " Better luck next time!");
+        }
+        return "roll-dice";
+    }
+
+}
