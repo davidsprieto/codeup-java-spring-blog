e06e26f18f6aef3e14891b18ca74993e8927244a
David Prieto <david.s.prieto1@gmail.com>
2022-07-12 11:29:04 -0500

    Added viewport to header.

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
@@ -13,10 +14,11 @@ public class RollDiceController {
 
     @GetMapping("/roll-dice")
     public String showGuessForm() {
-        return "posts/roll-dice";
+        return "roll-dice";
     }
 
     @PostMapping("/roll-dice")
+    @ResponseBody
     public String showGuess(@RequestParam(name = "guess") int guess, Model model) {
         Random random = new Random();
         int dice = random.nextInt(7-1) + 1;
@@ -26,7 +28,7 @@ public class RollDiceController {
         } else {
             model.addAttribute("dice", "The dice rolled on " + dice + "!" + " Better luck next time!");
         }
-        return "posts/roll-dice";
+        return "roll-dice";
     }
 
 }
